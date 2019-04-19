12f62a2c3306af29c5606de44f9059cbb82e19a4
César Felipe Pineda Ortiz <cfpinedao@unal.edu.co>
2019-04-18 20:24:58 -0500

    Updates

---
+++ precache-manifest.b33897d2d9e379f1ae56cb5febdc1f2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7f62d03ac3517d0e62f",
+    "url": "/profilepage/static/css/main.95ea4c5c.chunk.css"
+  },
+  {
+    "revision": "d7f62d03ac3517d0e62f",
+    "url": "/profilepage/static/js/main.23038c1c.chunk.js"
+  },
+  {
+    "revision": "69b31fde9829a875df5a",
+    "url": "/profilepage/static/js/runtime~main.55ab823d.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/profilepage/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profilepage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d554ec9883ee51ba8816f90a190f12e9",
+    "url": "/profilepage/index.html"
+  }
+];
