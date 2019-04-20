158de86525fb5c94732aa691cf5ecd674de46d1f
César Felipe Pineda Ortiz <cfpinedao@unal.edu.co>
2019-04-20 16:39:47 -0500

    oo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import escudo from './img/escudo.png';
+import republica from './img/republica.png';
 import './App.css';
+import Navigation from './components/nav'
+import InfoPerfil from './components/infoperfil';
+import logo from './img/logo.png';
+import Perfil from './components/perfil';
+import Footer from './components/footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        
+        <div className="row">
+          <img src={logo} className="" alt="reclutamiento"/>
+          <div className="ml-auto">
+          <img src={escudo} className="App-logo ml-auto" alt="logo" />
+          <img src={republica} className="App-logo ml-auto" alt="republica"/>
+          </div>
+          
+        </div>
+
         </header>
+        <Navigation/>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="carta col-sm-2">
+              <InfoPerfil/>
+            </div>
+            <div className="col-sm-8 ">
+              <Perfil/>
+            </div>
+ 
+          </div>
+          
+        </div>
+      
+            
+               
+
+              
+     
+        
       </div>
     );
   }
